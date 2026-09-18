4a68742eeb0b04bd8bfe486f288c150c1186ce8b
ndurve06 <neerav.durve@gmail.com>
2026-09-18 21:07:47 +0100

    Solution Accepted

---
+++ sWAP_cASE.py
@@ -0,0 +1,14 @@
+def swap_case(s):
+    result = ""
+    for i in range(len(s)):
+        if s[i].isupper():
+            result += s[i].lower()
+        else:
+            result += s[i].upper()
+            
+    return result
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
